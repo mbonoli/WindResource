274585706f4da7b52fd99fecd3ac6201edfe1632
mbonoli <mbonoli@gmail.com>
2014-07-10 02:41:58 -0300

    v3

---
+++ ui.R
@@ -0,0 +1,59 @@
+#' @title Complete numbers
+#' 
+#' @description
+#' Function to Complete numbers
+#' 
+#' @param data a list of paramenters.
+#' @param charnum number of digits.
+#' @return parameter's value extracted.
+#' @author Valeria Gogni, Mariano Bonoli, Ruben Bufanio, Diego Edwards
+#' @export
+#' 
+#' 
+shinyUI(pageWithSidebar(
+  
+  # Application title
+  headerPanel("Wind Resource"),
+  
+  # Sidebar with controls to select the variable to plot against mpg
+  # and to specify whether outliers should be included
+  sidebarPanel(
+    
+    selectInput("SELanalysis","Analysis type:",list(
+                        "Plots" = "plots", 
+                        "Turbulence" = "turbulence",
+                        "Fit" = "fit", 
+                        "Power Curve" = "pc")) ,
+    uiOutput("UIplottype"),
+    uiOutput("UIturbinetype"),
+    uiOutput("UIwtg"),
+    uiOutput("UIplotopt"),
+    uiOutput("UIplotby"),
+    uiOutput("UIbin"),
+    uiOutput("UIdates"),
+    downloadButton('dldat', 'Download Table'),
+    downloadButton("dlCurPlot", "Download Plot")
+
+  ),
+ 
+  # Show the caption and plot of the requested variable against mpg
+  mainPanel(
+    uiOutput("UItabs")
+#     h3(textOutput("caption")),
+#     tabsetPanel(
+#       tabPanel("Plot",
+#                h4(textOutput("captionP1")),
+#                plotOutput("plot1"),
+#                h4(textOutput("captionP2")),
+#                plotOutput("plot2")
+#       ),
+#       tabPanel("Tables",
+#                h4(textOutput("captionT1")),
+#                htmlOutput("table1"),
+#                h4(textOutput("captionT2")),
+#                htmlOutput("table2"),
+#                h4(textOutput("captionT3")),
+#                htmlOutput("table3")             
+#       )
+#     )
+)))
